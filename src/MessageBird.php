f65c5ec5775b0119021edcc668874338165ecc24
Mattijs Wit <mattijs@functioneelwit.nl>
2018-01-30 19:51:28 +0100

    remove vendor

--- src/MessageBird.php
+++ src/MessageBird.php
@@ -4,12 +4,22 @@ namespace FunctioneelWit\LaravelMessageBird;
 
 class MessageBird
 {
+    protected $apiKey;
+    protected $fromName;
+
     /**
      * Create a new Skeleton Instance
      */
     public function __construct()
     {
+        if(! $this->apiKey)
+        {
+            abort(501, 'Please add Messagebird API key to environment file (MESSAGE_BIRD_API_KEY).');
+        }
+
         // constructor body
+        $this->apiKey = env('MESSAGE_BIRD_API_KEY');
+        $this->fromName = env('MESSAGE_BIRD_FROM_NAME', config('app.name'));
     }
 
     /**
@@ -21,10 +31,10 @@ class MessageBird
      */
     public function sendSMS($phoneNumber, $message)
     {
-        $MessageBird = new \MessageBird\Client(env('MESSAGE_BIRD_API_KEY', 'please_set_api_key'));
+        $MessageBird = new \MessageBird\Client($this->apiKey);
 
         $Message = new \MessageBird\Objects\Message();
-        $Message->originator = env('MESSAGE_BIRD_FROM_NAME', config('app.name'));
+        $Message->originator = $this->fromName;
         $Message->recipients = $phoneNumber;
         $Message->body = $message;
         
